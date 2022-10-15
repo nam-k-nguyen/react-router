3daffc3a8dddb26ff68f650e7070d11e76250b40
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:35:01 -0400

    Update App.js
    Add comments

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import GalleryRoutes from "./components/GalleryRoutes";
 function App() {
   return (
     <>
+      {/* Navigation Bar */}
       <nav className='navbar'>
         <NavLink end to="/"><div className="link">Home</div></NavLink>
         <NavLink end to="/gallery"><div className="link">Gallery</div></NavLink>
       </nav>
+      {/* Main Routes */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/gallery/*' element={<GalleryRoutes />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
+      {/* Footer Routes */}
       <Routes >
         <Route path='/' element={''} />
         <Route path='/gallery' element={<GalleryFooter />} />
