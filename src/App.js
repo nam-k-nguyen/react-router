c07848f566eab489438aa7edf8c40e519d9fbebb
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:34:22 -0400

    Update App.js
    Add nested routes for gallery and create the navigation bar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ import GalleryRoutes from "./components/GalleryRoutes";
 function App() {
   return (
     <>
+      <nav className='navbar'>
+        <NavLink end to="/"><div className="link">Home</div></NavLink>
+        <NavLink end to="/gallery"><div className="link">Gallery</div></NavLink>
+      </nav>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/gallery/*' element={<GalleryRoutes />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
