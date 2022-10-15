624e02f8d5e25687d802784acb337492b324497d
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:27:27 -0400

    Move Home.js
    Move Home.js to components directory and change text content

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home_container'>
+      This is a mini project I built to learn about React Router v6
+    </div>
+  )
+}
