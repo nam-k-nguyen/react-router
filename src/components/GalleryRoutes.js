6734534c48596363d20b0f035792af5d504a59ea
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:50:23 -0400

    Update GalleryRoutes.js
    Create a nested routing component for gallery

--- src/components/GalleryRoutes.js
+++ src/components/GalleryRoutes.js
@@ -1,1 +1,18 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Gallery from './Gallery';
+import Image from './Image'
+import NotFound from "./NotFound";
+import GalleryLayout from "./GalleryLayout";
 
+export default function GalleryRoutes() {
+    return (
+        <Routes>
+            <Route element={<GalleryLayout />}>
+                <Route index element={<Gallery />} />
+                <Route path=':id/:name' element={<Image />} />
+                <Route path='*' element={<NotFound />}></Route>
+            </Route>
+        </Routes>
+    )
+}
