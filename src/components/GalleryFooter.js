350309e48b356c37f7ba30193fedc438a7dad12a
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:43:43 -0400

    Update GalleryFooter.js
    Add text content and class name

--- src/components/GalleryFooter.js
+++ src/components/GalleryFooter.js
@@ -1,1 +1,12 @@
+import React from 'react'
 
+export default function GalleryFooter() {
+    return (
+        <footer className='gallery_footer'>
+            <div>
+                Did you know: All of the keyboards on this site are of XDA profile.
+                Simply put, they are flat, stylish, and favored by many!
+            </div>
+        </footer>
+    )
+}
