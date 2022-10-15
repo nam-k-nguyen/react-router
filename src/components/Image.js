464ffd53313900a82a2597059068545da61b1617
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:49:47 -0400

    Update Image.js
    Render a single image based on url, or if there is no matching images, render an text indicate invalid url

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,1 +1,18 @@
+import React from 'react'
+import { useParams, useOutletContext } from 'react-router-dom'
 
+export default function Image() {
+    const { id, name } = useParams()
+    const { base_url, images } = useOutletContext();
+
+    const foundImage = images.find(image => image.id === id && image.name === name);
+
+    return (
+        <div className='image_container'>
+            {foundImage ?
+                <img src={`${base_url}/${id}/${name}.jpg`} alt={name} /> :
+                <div className='invalid_image'>Invalid Image URL</div>
+            }
+        </div>
+    )
+}
