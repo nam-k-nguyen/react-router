7a7d75ce197eef894e2ff43f4c2bbc5d64c3d422
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:35:38 -0400

    Update NotFound.js
    Change text content and add class name for styling

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,1 +1,9 @@
+import React from 'react'
 
+export default function NotFound() {
+  return (
+    <div className='not_found_container'>
+        The page you requested does not exist, or has been deleted
+    </div>
+  )
+}
