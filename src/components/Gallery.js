0131e4558aff1460255366468c61e8eccc969d87
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-15 07:48:27 -0400

    Update Gallery.js
    Render all image pass down from outlet and a link to each of them linking to their respective image page.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import { Link, useOutletContext } from 'react-router-dom';
 
+export default function Gallery() {
+  const { base_url, images } = useOutletContext();
+
+  return (
+    <div className='gallery_container'>
+      {images.map(image => {
+        return (
+          <div className='gallery_image' key={image.id}>
+            <Link to={`/gallery/${image.id}/${image.name}`}>
+              <img src={`${base_url}/${image.id}/${image.name}.jpg`} alt={image.name} />
+            </Link>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
